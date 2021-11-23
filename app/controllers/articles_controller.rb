a954e47e96cd337926b8b57f9e6b9cb9101abdc0
Vlad Beil <vladislav.beil@cloudinary.com>
2021-11-23 15:24:26 +0200

    add article create and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,38 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :author, :description))
+    if @article.save
+      #redirect_to article_path(@article)
+      flash[:notice] = "Article was created succssfully!"
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :author, :description))
+      #redirect_to article_path(@article)
+      flash[:notice] = "Article was updated succssfully!"
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
   def show
     #byebug
     @article = Article.find(params[:id])
